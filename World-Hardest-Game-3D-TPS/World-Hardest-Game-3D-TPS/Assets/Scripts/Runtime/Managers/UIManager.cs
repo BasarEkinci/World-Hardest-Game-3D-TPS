5df2d58655515bed77d5e6a30f78866c422fa9b5
BasarEkinci <basarekinci02@outlook.com>
2023-11-22 23:02:45 +0300

    New Models Added

--- World-Hardest-Game-3D-TPS/World-Hardest-Game-3D-TPS/Assets/Scripts/Runtime/Managers/UIManager.cs
+++ World-Hardest-Game-3D-TPS/World-Hardest-Game-3D-TPS/Assets/Scripts/Runtime/Managers/UIManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Runtime.Signals;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ namespace Runtime.Managers
 {
     public class UIManager : MonoBehaviour
     {
+        [SerializeField] private GameObject winPanel;
         private void OnEnable()
         {
             CoreGameSignals.Instance.OnLevelComplete += OnLevelComplete;
@@ -20,10 +22,12 @@ namespace Runtime.Managers
             CoreGameSignals.Instance.OnClearActiveLevel?.Invoke();
             CoreGameSignals.Instance.OnLoadLevel?.Invoke();
             CoreGameSignals.Instance.OnNextLevel?.Invoke();
+            winPanel.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack);
         }
 
         private void OnLevelComplete()
         {
+            winPanel.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
             Debug.Log("Level Complete");
         }
     }
